0a4174d294a786f3f002ff4b800f1a18ef6f59c8
Hunter Rudnet <hunter.rudnet@gmail.com>
2022-02-21 13:40:51 -0500

    prettify code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <Button color={showAdd ? 'red': 'green'} text={showAdd ? 'Close' :'Add'} onClick={onAdd}/>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task tracker',
-} 
+  title: "Task tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 // CSS in JS
 /* const headingStyle = {
@@ -24,4 +28,4 @@ Header.propTypes = {
     backgroundColor: 'black'
 } */
 
-export default Header
+export default Header;
