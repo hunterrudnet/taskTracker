8284fb201897aa62d813814e669817458dfe6a56
Hunter Rudnet <hunter.rudnet@gmail.com>
2022-02-21 13:35:52 -0500

    add stuff

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -19,6 +20,15 @@ function App() {
     }
 ])
 
+// Add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000 + 1)
+  const newTask = {
+    id, ...task
+  }
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter(task => task.id !== id))
@@ -32,8 +42,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-    <Header/>
-    <AddTask />
+    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+    {showAddTask  && <AddTask onAdd={addTask}/>}
     {tasks.length > 0 ? 
     <Tasks tasks={tasks} onDelete={deleteTask}
       onToggle={toggleReminder}
